a1279564b12d9cb68ad301b872fb539c98614ee6
Peter Williams <peter.williams@papercut.cm>
2016-01-05 21:24:07 +1100

    Use this script to check your python library versions

--- version.py
+++ version.py
@@ -6,10 +6,14 @@ import sys
 import numpy as np
 import pandas as pd
 import matplotlib
+from multiprocessing import cpu_count
+
 
 print(__doc__)
 print('    python    : %s.%s.%s' % sys.version_info[:3])
 print('    numpy     : %s' % np.__version__)
 print('    matplotlib: %s' % matplotlib.__version__)
 print('    pandas    : %s '% pd.__version__)
+print()
+print('    cpu_count : %s '% cpu_count())
 print()
